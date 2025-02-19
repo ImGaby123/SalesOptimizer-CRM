982b1e0da422ba025e1f9413b10952d76a3f7036
Administrator <Administrator@DESKTOP-HU962IE>
2025-02-19 22:22:38 +0800

    removed pycache in repository

--- CRM/main.py
+++ CRM/main.py
@@ -1,8 +1,24 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication
-from models.models_authentication import AuthenticationSystem
+#from models.models_authentication import AuthenticationSystem
+from models.models_authentication import MainWindow
 
+# Initialize the application
 app = QApplication(sys.argv)
-window = AuthenticationSystem()  # Siguraduhin na ito ay subclass ng QMainWindow
+
+# Load and apply QSS stylesheet
+qss_file = "views/qss/style.qss"
+
+if os.path.exists(qss_file):
+    with open(qss_file, "r") as file:
+        app.setStyleSheet(file.read())
+else:
+    print(f"Warning: QSS file '{qss_file}' not found. The application will run without styles.")
+
+# Create and show the main window
+window = MainWindow()  # Ensure this is a subclass of QMainWindow
 window.show()
+
+# Run the application event loop
 app.exec()
--- CRM/views/py/ui_contacts.py
+++ CRM/views/py/ui_contacts.py
@@ -1975,7 +1975,7 @@ class Ui_contacts(object):
 
         self.retranslateUi(contacts)
 
-        self.stackedWidget.setCurrentIndex(2)
+        self.stackedWidget.setCurrentIndex(4)
         self.stackedWidget_2.setCurrentIndex(1)
         self.stackedWidget_3.setCurrentIndex(2)
         self.stackedWidget_4.setCurrentIndex(4)
