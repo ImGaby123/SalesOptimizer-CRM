381ca1f4030bb83faa5c002d2fd5f7fd83adafde
ImGaby123 <gabyplaza123@gmail.com>
2025-04-11 16:24:26 +0800

    Setup the initial DB Connection and Sample Query for the Lead Prioritization Feature

---
+++ SalesOptimizer/db_connection.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+
+class db_connection:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="127.0.0.1", 
+            user="root", 
+            password="BORRIS", 
+            database="crm", 
+            port=3306
+            )
+
+
+
+# For Debugging Purposes
+dog = db_connection()
+print("Connection Credentials: ",dog.conn)
+
+
---
+++ SalesOptimizer/query_test.py
@@ -0,0 +1,36 @@
+from db_connection import db_connection
+
+# Connection
+db = db_connection()
+conn = db.conn
+
+# cursor1
+cursor1 = conn.cursor()
+cursor2 = conn.cursor()
+
+# Attributes
+cursor1.execute("select * from icp ORDER BY icp_id ASC;")
+attributes = cursor1.fetchall()
+
+# Rules
+cursor2.execute("select * from icp_rules " \
+                "ORDER BY icp_icp_id ASC," \
+                "attribute_score DESC;")
+rules = cursor2.fetchall()
+
+
+# Print
+print("------ ICP Attributes ------")
+for attribute in attributes:
+    print(attribute[1])
+
+print("------ ICP Rules ------")
+for rule in rules:
+    attribute_name = next((attribute[1] for attribute in attributes if attribute[0] == rule[0]), None)
+
+    print("{} = {} : {}".format(attribute_name,rule[1], rule[2]))
+
+# Close
+cursor1.close()
+cursor2.close()
+conn.close()
